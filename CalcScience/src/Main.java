8fca32a432dcf6fc3f68cf80f667039db6c85f9a
Mahdi Hasan <43575088+Mahdi-Hasan@users.noreply.github.com>
2019-04-10 09:45:01 +0600

    Add files via upload

---
+++ CalcScience/src/Main.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+
+    public Main() {
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+}
